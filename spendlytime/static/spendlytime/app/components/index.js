498cfee65c769568ddcca9021f1f14f64c2e8cae
Michał Gołda <michalgolda@vp.pl>
2020-04-06 14:44:41 +0200

    Add index.js to components so as to export all

---
+++ spendlytime/static/spendlytime/app/components/index.js
@@ -0,0 +1,7 @@
+import HelloComponent from './HelloComponent/HelloComponent.container';
+import RouterComponent from './RouterComponent/RouterComponent.container';
+
+export {
+    HelloComponent,
+    RouterComponent
+}
