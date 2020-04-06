ea6e7446becd2c03e2e276a2aa42142b8cbd4fc0
Michał Gołda <michalgolda@vp.pl>
2020-04-06 21:14:25 +0200

    Add test trace serializer but not implemented test logic

---
+++ tests/api/serializers/test_trace.py
@@ -0,0 +1,8 @@
+from tests.base import BaseTestCase, skip
+
+
+class TestTraceSeraializer(BaseTestCase):
+
+    @skip("Not implemented. TraceSerializer is required request. I dont know to solve that.")
+    def test_serialize(self):
+        pass
