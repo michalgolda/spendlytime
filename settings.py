1c991e4e0a08703368a7d35f542f5568ac975d03
Michał Gołda <michalgolda@vp.pl>
2020-04-06 23:12:39 +0200

    Change root urls module

--- settings.py
+++ settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'urls'
+ROOT_URLCONF = 'spendlytime.urls'
 
 TEMPLATES = [
     {
--- spendlytime/urls.py
+++ spendlytime/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('spendlytime.web.urls')),
+    path('admin/', admin.site.urls),
     path('api/', include('spendlytime.api.urls'))
 ]
