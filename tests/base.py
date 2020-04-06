dd5f8c0fe11e37f6e4eda77c639031e457759fbd
Michał Gołda <michalgolda@vp.pl>
2020-04-06 21:11:26 +0200

    Add BaseAPITestCase class
    This class is wrapper on APITestCase class. Class is contain a implementation of setUp for creating a user if not exist and login client.
    
    tearDown func is delete last create user and del user object from contain in this class.

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,26 @@
-from unittest import TestCase
+from unittest import TestCase, skip
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
 
 class BaseTestCase(TestCase):
+    """The wrapper for TestCase class"""
     pass
+
+
+class BaseAPITestCase(APITestCase):
+    """The wrapper for APITestCase class"""
+
+    def setUp(self):
+        """Create test user if not exist for set up test case and login client"""
+        self.user = None
+        has_user = User.objects.filter(username="test")
+        if not has_user:
+            self.user = User.objects.create_user(
+                username="test", password="test")
+            self.client.login(username="test", password="test")
+
+    def tearDown(self):
+        """Delete user object on tear down test case"""
+        self.user.delete()
+        del self.user
