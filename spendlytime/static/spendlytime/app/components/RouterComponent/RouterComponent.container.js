8b53d73506888595d837250abef6e27b602ead08
Michał Gołda <michalgolda@vp.pl>
2020-04-06 14:43:22 +0200

    Add router component for rendering all routes

---
+++ spendlytime/static/spendlytime/app/components/RouterComponent/RouterComponent.container.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+function RouterComponent(props) {
+    const { routes } = props;
+    const routesItems = routes.map((route) => (
+        <Route
+            key={route.path}
+            path={route.path}
+            component={route.component}
+            exact={route.exact ? true : false}
+        />
+    ));
+
+    return (
+        <BrowserRouter>
+            <Switch>{routesItems}</Switch>
+        </BrowserRouter>
+    );
+}
+
+RouterComponent.propTypes = {
+    routes: PropTypes.object.isRequired,
+};
+
+export default RouterComponent;
