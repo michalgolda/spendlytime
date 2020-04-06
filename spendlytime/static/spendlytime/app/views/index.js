474010f81cd1a069908534caf85540a2720d9222
Michał Gołda <michalgolda@vp.pl>
2020-04-06 14:42:35 +0200

    Add index.js so as to all views export

---
+++ spendlytime/static/spendlytime/app/views/index.js
@@ -0,0 +1,9 @@
+import HomeView from './HomeView/HomeView.container';
+
+// Errors views
+import NotFoundView from './NotFoundView/NotFoundView.container';
+
+export {
+    HomeView,
+    NotFoundView
+}
