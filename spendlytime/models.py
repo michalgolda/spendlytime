3d079f4e67ed546e4ce403c70a58265a28cc495b
Michał Gołda <michalgolda@vp.pl>
2020-04-06 18:49:32 +0200

    Refactor Trace model class
    Add __str__ function to string represenation using str() func

--- spendlytime/models.py
+++ spendlytime/models.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 
 
 class Trace(models.Model):
+    """
+    Base model of Trace
+    """
     trace_url = models.CharField(max_length=150)
     trace_time = models.TimeField()
 
@@ -10,3 +13,12 @@ class Trace(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """The function is return trace url for this model
+
+        Example:
+            > str(trace_obj)
+            > https://google.com
+        """
+        return f"{self.trace_url}"
