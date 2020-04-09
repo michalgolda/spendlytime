088f969146d63e608835fb3e591df3c0c5d928a9
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:27:17 +0200

    Add logo component

---
+++ spendlytime/static/spendlytime/app/components/LogoComponent/LogoComponent.container.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as S from './LogoComponent.styles';
+
+export default function Logo(){
+    return(
+        <S.Container>
+            <S.Text>Spendlytime.com</S.Text>
+        </S.Container>
+    );
+}
---
+++ spendlytime/static/spendlytime/app/components/LogoComponent/LogoComponent.styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { colors } from '../../utils/styles/colors';
+
+export const Container = styled.div`
+    width: 280px;
+    height: 38px;
+    display: inline-block;
+`;
+
+export const Text = styled.h1`
+    color: ${colors.green};
+    font-size: 2.1rem;
+    font-weight: bold;
+`;
