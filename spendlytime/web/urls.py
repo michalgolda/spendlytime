b25e58f7e9a6020f6d0b207054fee85c42657463
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:31:42 +0200

    Add login path to web urls

--- spendlytime/web/urls.py
+++ spendlytime/web/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from spendlytime.web.frontend.react import ReactView
 
 urlpatterns = [
-    path('', ReactView.as_view())
+    path('', ReactView.as_view()),
+    path('auth/', include([
+        path('login/', ReactView.as_view())
+    ]))
 ]
