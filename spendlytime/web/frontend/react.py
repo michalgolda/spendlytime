d85c6355e645a30f1a442ca2f3e7e166c8b356a5
Michał Gołda <michalgolda@vp.pl>
2020-04-09 20:01:09 +0200

    Add GenericReactPage class and ReactMixin

--- spendlytime/web/frontend/react.py
+++ spendlytime/web/frontend/react.py
@@ -1,4 +1,19 @@
-from django.views.generic import TemplateView
+from django.conf import settings
+from django.middleware.csrf import get_token as get_csrf_token
 
-class ReactView(TemplateView):
-    template_name = "spendlytime/base/react.html"
+from spendlytime.web.frontend.base import BaseView
+from spendlytime.web.helpers import render_to_response
+
+
+class ReactMixin(object):
+    def handle_react(self, request):
+        context = {"CSRF_COOKIE_NAME": settings.CSRF_COOKIE_NAME}
+
+        get_csrf_token(request)
+
+        return render_to_response("spendlytime/bases/react.html", context=context, request=request)
+
+
+class GenericReactPage(BaseView, ReactMixin):
+    def handle(self, request, **kwargs):
+        return self.handle_react(request)
