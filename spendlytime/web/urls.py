f246b276f92934c7115d4b68b2a8618a6856bc88
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:47:20 +0200

    Add register path to web urls

--- spendlytime/web/urls.py
+++ spendlytime/web/urls.py
@@ -4,6 +4,7 @@ from spendlytime.web.frontend.react import ReactView
 urlpatterns = [
     path('', ReactView.as_view()),
     path('auth/', include([
-        path('login/', ReactView.as_view())
+        path('login/', ReactView.as_view()),
+        path('register/', ReactView.as_view())
     ]))
 ]
