cd103503ed6c2e425a6d3755af5f5307f8037910
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:27:43 +0200

    Set global font

--- spendlytime/static/spendlytime/app/utils/styles/global.js
+++ spendlytime/static/spendlytime/app/utils/styles/global.js
@@ -5,4 +5,9 @@ export const GlobalStyles = createGlobalStyle`
         padding: 0;
         margin: 0;
     }
+
+    body{
+        min-height: 100vh;
+        font-family: 'Roboto', sans-serif;
+    }
 `;
