ec212f8bf5d3a54c538732658a8ee6bb62aa69a9
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:27:51 +0200

    Add login view to export

--- spendlytime/static/spendlytime/app/views/index.js
+++ spendlytime/static/spendlytime/app/views/index.js
@@ -1,9 +1,11 @@
 import HomeView from './HomeView/HomeView.container';
+import LoginView from './LoginView/LoginView.container';
 
 // Errors views
 import NotFoundView from './NotFoundView/NotFoundView.container';
 
 export {
     HomeView,
+    LoginView,
     NotFoundView
 }
