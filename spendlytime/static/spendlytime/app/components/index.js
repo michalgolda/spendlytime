1c5aa1482d2d9dff5f40cdebc0cad3f9871279bd
Michał Gołda <michalgolda@vp.pl>
2020-04-09 16:27:02 +0200

    Add logo component to export

--- spendlytime/static/spendlytime/app/components/index.js
+++ spendlytime/static/spendlytime/app/components/index.js
@@ -1,7 +1,9 @@
 import HelloComponent from './HelloComponent/HelloComponent.container';
 import RouterComponent from './RouterComponent/RouterComponent.container';
+import LogoComponent from './LogoComponent/LogoComponent.container';
 
 export {
+    LogoComponent,
     HelloComponent,
     RouterComponent
 }
