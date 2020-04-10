fef48ad68c18e159cea03ff498ecd725fd0e2a0d
Michał Gołda <michalgolda@vp.pl>
2020-04-10 15:39:14 +0200

    Add email authorization backend because default backend is required username authentication

---
+++ spendlytime/web/backends.py
@@ -0,0 +1,21 @@
+from typing import Union
+
+from django.http import HttpRequest
+
+from django.contrib.auth.models import User
+from django.contrib.auth.backends import BaseBackend
+from django.contrib.auth.hashers import check_password
+
+
+class EmailAuthBackend(BaseBackend):
+    """
+    The backend is using email and password for authentication user
+    """
+
+    def authenticate(self, request: HttpRequest, email: str = None, password: str = None) -> Union[User, None]:
+        try:
+            user = User.objects.get(email=email)
+            if user.check_password(password):
+                return user
+        except User.DoesNotExist:
+            return None
