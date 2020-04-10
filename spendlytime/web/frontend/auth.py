ec6d0fcc4c7a840ecc7e9939b757b324bed09108
Michał Gołda <michalgolda@vp.pl>
2020-04-10 15:40:26 +0200

    Add login and register view

--- spendlytime/web/frontend/auth.py
+++ spendlytime/web/frontend/auth.py
@@ -3,17 +3,20 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 
 from spendlytime.web.frontend.base import BaseView
-from spendlytime.web.forms.authentication import AuthtenticationForm
-from spendlytime.web.forms.user import UserCreationForm
+from spendlytime.web.forms.auth import LoginForm, RegisterForm
 
 
 class AuthLoginView(BaseView):
+    """
+    The view is implementing login process
+    """
+
 
     def respond_login(self, request, context, **kwargs):
         return self.respond("spendlytime/login.html", context)
 
-    def get_form(self, request):
-        return AuthtenticationForm(request.POST)
+    def get_form(self, request=None):
+        return LoginForm(request.POST if request else None)
 
     def get(self, request, *args, **kwargs):
         context = {
@@ -23,9 +26,10 @@ class AuthLoginView(BaseView):
         return self.respond_login(request, context, **kwargs)
 
     def post(self, request, *args, **kwargs):
-        form = AuthtenticationForm(request.POST)
+        form = self.get_form(request)
         if form.is_valid():
-            user = authenticate(request, username=request.POST.get("email"), password=request.POST.get("password"))
+            user = authenticate(request, email=request.POST.get(
+                "email"), password=request.POST.get("password"))
             if user is not None:
                 login(request, user)
 
@@ -37,20 +41,40 @@ class AuthLoginView(BaseView):
                     "form": form
                 }
                 return self.respond_login(request, context, **kwargs)
-
+        else:
+            form = self.get_form(request)
+            context = {
+                "form": form
+            }
+            return self.respond_login(request, context, **kwargs)
 
 
 class AuthRegisterView(BaseView):
+    """
+    This view is implementing user creation process
+    """
 
-    def respond_login(self, request, context, **kwargs):
+    def respond_register(self, request, context, **kwargs):
         return self.respond("spendlytime/register.html", context)
 
-    def get_form(self, request):
-        return UserCreationForm(request.POST)
+    def get_form(self, request=None):
+        return RegisterForm(request.POST if request else None)
 
     def get(self, request, *args, **kwargs):
         context = {
             "form": self.get_form(request)
         }
 
-        return self.respond_login(request, context, **kwargs)
+        return self.respond_register(request, context, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form(request)
+        if form.is_valid():
+            user = form.save()
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            form = self.get_form(request)
+            context = {
+                "form": form
+            }
+            return self.respond_register(request, context, **kwargs)
