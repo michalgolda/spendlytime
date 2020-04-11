c0aa3a7f63533f1e4e11ab66a2b1f9a8b76fb9c2
Michał Gołda <michalgolda@vp.pl>
2020-04-11 15:11:41 +0200

    Add style utils

---
+++ spendlytime/static/spendlytime/app/utils/styles/colors.js
@@ -0,0 +1,6 @@
+export const colors = {
+    green: "#00D856",
+    black: "#323232",
+    grey: "#FF005C",
+    red: "#FF005C",
+};
---
+++ spendlytime/static/spendlytime/app/utils/styles/index.js
@@ -0,0 +1,9 @@
+import { colors } from './colors';
+import { device } from './media';
+import { GlobalStyles } from './global';
+
+export {
+    colors,
+    device,
+    GlobalStyles
+}
