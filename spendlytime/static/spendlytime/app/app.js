d8d3c5351f9092efd666ac860f29f6e8f6c14f84
Michał Gołda <michalgolda@vp.pl>
2020-04-11 15:11:52 +0200

    Change import path

--- spendlytime/static/spendlytime/app/app.js
+++ spendlytime/static/spendlytime/app/app.js
@@ -2,7 +2,7 @@ import React from "react";
 import { RouterComponent as Router } from "./components";
 
 import { routes } from "./routes";
-import { GlobalStyles } from './utils/styles/global';
+import { GlobalStyles } from './utils/styles';
 
 export default function App() {
     return (
