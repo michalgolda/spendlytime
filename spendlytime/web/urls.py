a6de4b932169f349e8e8f85cc90d61307de2b6d3
Michał Gołda <michalgolda@vp.pl>
2020-04-11 19:33:04 +0200

    Change url

--- spendlytime/web/urls.py
+++ spendlytime/web/urls.py
@@ -4,7 +4,7 @@ from spendlytime.web.frontend.react import GenericReactPage
 from spendlytime.web.frontend.auth import AuthLoginView, AuthRegisterView
 
 urlpatterns = [
-    path('traces/', GenericReactPage.as_view(), name="main"),
+    path('timer/', GenericReactPage.as_view(), name="main"),
     path('auth/', include([
         path('login/', AuthLoginView.as_view(), name="auth-login"),
         path('register/', AuthRegisterView.as_view(), name="auth-register")
