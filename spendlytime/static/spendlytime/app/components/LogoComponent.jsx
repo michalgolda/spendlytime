d7124257e77c5b5e0cc3be85f921d8710cc39ce6
Michał Gołda <michalgolda@vp.pl>
2020-04-12 14:46:13 +0200

    Remove logo component

--- spendlytime/static/spendlytime/app/components/LogoComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Logo(){
-    return(
-        <div className="logo">
-            <h1 className="logo__text">Spendlytime.com</h1>
-        </div>
-    );
-}
