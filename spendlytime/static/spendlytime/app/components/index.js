5e4dd1493d3f4c6cb08373e379b3375adc10be73
Maklero <michalgolda@vp.pl>
2020-04-15 01:23:22 +0200

    Remove LinkComponent

--- spendlytime/static/spendlytime/app/components/index.js
+++ spendlytime/static/spendlytime/app/components/index.js
@@ -1,4 +1,3 @@
-import LinkComponent from './LinkComponent';
 import RouteComponent from './RouteComponent';
 import RouterComponent from './RouterComponent';
 import SidebarComponent from './SidebarComponent/SidebarComponent';
@@ -6,7 +5,6 @@ import LoadingComponent from './LoadingComponent/LoadingComponent';
 import TraceItemComponent from './TraceItemComponent/TraceItemComponent';
 
 export {
-    LinkComponent,
     RouteComponent,
     RouterComponent,
     SidebarComponent,
