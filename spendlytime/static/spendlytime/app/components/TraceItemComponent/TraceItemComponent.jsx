56e977546413891bb1bdff2f79ecda1c8e19ff1b
Maklero <michalgolda@vp.pl>
2020-04-19 21:07:12 +0200

    Add timer integration on  backend

--- spendlytime/static/spendlytime/app/components/TraceItemComponent/TraceItemComponent.jsx
+++ spendlytime/static/spendlytime/app/components/TraceItemComponent/TraceItemComponent.jsx
@@ -3,21 +3,60 @@ import PropTypes from 'prop-types';
 import * as S from './TraceItemComponent.styles';
 
 import { connect } from 'react-redux';
-import { traceActions } from '../../actions';
+import { traceActions, timeEntryActions } from '../../actions';
 
 import { faPlay, faStop, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function TraceItemComponent(props){
 
-    const { url, time, id } = props;
+    const { url, duration, id } = props;
 
     const [isShowActions, setShowActions] = useState(false);
-    const [isTimer, setTimer] = useState(false);
+    const [timerInterval, setTimerInterval] = useState(false);
+    const [timerDuration, setTimerDuration] = useState(duration);
+    const [isRunTimer, setRunTimer] = useState(false);
 
     function handleDelete(e){
         e.preventDefault();
 
         props.deleteTrace(id);
+    }
+
+    function startTimer(e){
+        e.preventDefault();
+
+        setRunTimer(true);
+
+        const date = new Date();
+        const data = {
+            tid: props.id,
+            start: date.toJSON()
+        }
+        props.startTimeEntry(data);
+
+        const interval = setInterval(() => {
+            setTimerDuration(duration => duration + 1);
+        }, 1000);
+
+        setTimerInterval(interval);
+    }
+
+    function stopTimer(e){
+        e.preventDefault();
+
+        setRunTimer(false);
+
+        clearInterval(timerInterval);
+        setTimerInterval(null);
+
+        const timeEntryId = props.timeEntry.items[props.id].id;
+        const date = new Date();
+        const data = {
+            tid: props.id,
+            stop: date.toJSON(),
+            duration: timerDuration
+        }
+        props.stopTimeEntry(timeEntryId, data);
     }
 
     return(
@@ -28,7 +67,7 @@ function TraceItemComponent(props){
             >
                 <S.Trace>
                     <S.TraceUrl>{url}</S.TraceUrl>
-                    <S.TraceTime>{time}</S.TraceTime>
+                    <S.TraceTime>{timerDuration}</S.TraceTime>
                 </S.Trace>
                 {isShowActions &&
                     <S.Actions>
@@ -36,8 +75,8 @@ function TraceItemComponent(props){
                             <S.Action onClick={(e) => handleDelete(e)}>
                                 <S.DeleteActionIcon icon={faTrash} />
                             </S.Action>
-                            <S.Action onClick={() => isTimer ? setTimer(false) : setTimer(true)}>
-                                <S.PlayAndStopActionIcon icon={isTimer ? faStop : faPlay} />
+                            <S.Action onClick={(e) => isRunTimer ? stopTimer(e) : startTimer(e)}>
+                                <S.PlayAndStopActionIcon icon={isRunTimer ? faStop : faPlay} />
                             </S.Action>
                         </S.ActionsBox>
                     </S.Actions>
@@ -46,18 +85,16 @@ function TraceItemComponent(props){
         </>
     );
 }
-
 TraceItemComponent.propTypes = {
     url: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired
+    duration: PropTypes.number.isRequired
 }
-
 function mapState(state){
     return state;
 }
-
 const actionCreators = {
-    deleteTrace: traceActions.deleteTrace
+    deleteTrace: traceActions.deleteTrace,
+    startTimeEntry: timeEntryActions.startTimeEntry,
+    stopTimeEntry: timeEntryActions.stopTimeEntry
 }
-
 export default connect(mapState, actionCreators)(TraceItemComponent);
